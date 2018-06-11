414a1e8afa9aa4ee479d1512c497515c596f2320
unhooked52 <rohan.shelar28@gmail.com>
2018-06-11 16:25:57 +0530

    new check in

--- resource/js/app.js
+++ resource/js/app.js
@@ -73,10 +73,79 @@ var hotelController = (function () {
 
 var uiController = (function () {
 
+    var DOMStrings = {
+        getMenuBtn: 'get_menu_btn',
+        hotelIdTextBox: 'hotel_id',
+        checkInSection: 'checkin_selection',
+        menuSection: 'menu_section',
+    }
+
+    var displayMenu = function (menu) {
+        if (menu !== null) {
+
+            var html;
+            html = '<ol>'
+            menu.forEach(function (menuItem) {
+                html += '<li> Name : ' + menuItem.name + ' Description : ' + menuItem.desc + ' Price : ' + menuItem.price + '</li>'
+            });
+            html += '</ol>'
+            document.getElementById(DOMStrings.menuSection).insertAdjacentHTML('afterbegin', html);
+        } else {
+            //Menu not found
+        }
+    }
+
+    return {
+        DOMStrings: DOMStrings,
+        readHotelId: function () {
+            return document.getElementById(DOMStrings.hotelIdTextBox).value;
+        },
+        hideCheckInSection: function () {
+            document.getElementById(DOMStrings.checkInSection).hidden = true;
+        },
+        renderMenu: function (menu) {
+            displayMenu(menu);
+        }
+    }
 })()
 
 
 var appController = (function (hController) {
 
+    var menu;
 
+    var initalizeApplication = function () {
+        console.log("Application Initalized");
+
+        addEventHandlers();
+    }
+
+    var addEventHandlers = function () {
+        var DOMStrings = uiController.DOMStrings;
+        document.getElementById(DOMStrings.getMenuBtn).addEventListener('click', getMenuClicked);
+    }
+
+    //get Menu card
+    var getMenuClicked = function () {
+        // Read input from UI
+        var hotelId = parseFloat(uiController.readHotelId());
+        if (!isNaN(hotelId)) {
+            // Get menu object from Hotel controller
+            menu = hotelController.getMenu(hotelId);
+            console.log(menu);
+            // Hide Check in section
+            uiController.hideCheckInSection();
+            // Show check out section
+            // Render on UI
+            uiController.renderMenu(menu);
+        }
+    }
+
+    return {
+        init: function () {
+            initalizeApplication();
+        }
+    }
 })(hotelController)
+
+appController.init();
